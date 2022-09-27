b6d9942c8fa413f97803c483179147b9fb0dd408
Salvador Mejia <salvador.josemc@gmail.com>
2022-09-26 20:03:30 -0600

    Rebuild grid function and add grid size function.

--- app.js
+++ app.js
@@ -7,55 +7,53 @@
     let RGBColor = 'rgb('+x+','+y+','+z+')';
     console.log(RGBColor);   
 }*/
-function gridmaker() {
-    const button = document.getElementById('grid-size');
-    let divs;
-    let popup = function () {
-        parseInt(prompt('Squares per side:'));
+
+
+const container = document.getElementById('container');
+
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    container.style.width = '960px';
+    container.style.overflow = 'hidden';
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        
+        cell.style.backgroundColor = 'black';
+        cell.style.minwidth = '0';
+        cell.style.minheight = '0';
+        container.appendChild(cell).className = "grid-item";
+        cell.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = 'white';
+            setTimeout(() => {
+                event.target.style.backgroundColor = 'peachpuff';
+            }, 100);
+        }, false);
     }
-
-    button.addEventListener('click', popup);
-    grid();
-    
-
-    function grid() {
-        for (let i = 0; i < 256; i++) {
-            divs = document.createElement('div');
-            divs.style.height = '16px';
-            divs.style.width = '16px';
-            divs.style.backgroundColor = 'black';
-            document.getElementById('container').appendChild(divs);
-            divs.addEventListener('mouseover', (event) => {
-                event.target.style.backgroundColor = 'white';
-                setTimeout(() => {
-                    event.target.style.backgroundColor = 'peachpuff';
-                }, 100);
-            }, false);
-        }
-    };
-
-    function gridSize() {
-        if (popup.value <= 100) {
-            for (let i = 0; i < popup.value; i++) {
-                divs = document.createElement('div');
-                divs.style.height = '16px';
-                divs.style.width = '16px';
-                divs.style.backgroundColor = 'black';
-                document.getElementById('container').appendChild(divs);
-                divs.addEventListener('mouseover', (event) => {
-                    event.target.style.backgroundColor = 'white';
-                    setTimeout(() => {
-                        event.target.style.backgroundColor = 'peachpuff';
-                    }, 100);
-                }, false);
-            }
-        } else if (popup.value > 100) {
-            alert('Size limit is 100');
-        }
-    };
-    gridSize();
 };
-gridmaker();
+
+makeGrid(16, 16);
+
+function gridSize() {
+    const button = document.getElementById('grid-size');
+    button.addEventListener('click', () => {
+        document.querySelectorAll('grid-item').forEach(e => e.remove());
+        let userInput = prompt('Enter number of squares per side');
+        if (userInput <= 100) {
+            rows = userInput;
+            columns = userInput;
+            makeGrid(rows, columns);
+        } else if (userInput > 100) {
+            alert('Size limit max is 100');
+        } 
+    })
+};
+gridSize();
+
+
+
+
+
 
 
 
